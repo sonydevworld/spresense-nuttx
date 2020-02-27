6ee4432af541d5f51ca9d058d564d58f12c096f2
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2020-02-27 14:41:51 +0900

    Fix bug that SPI transaction cannot be performed
    Fix bug that SPI transaction cannot be performed
    when the reset packet and the send packet conflicted.
    Modified to discard the send packets if a conflict occurs.

--- drivers/modem/altair/altmdm_spi.c
+++ drivers/modem/altair/altmdm_spi.c
@@ -1234,16 +1234,11 @@ static int do_trxreset(FAR struct altmdm_dev_s *priv)
   ret = wait_receiverready(priv);
   if (ret >= 0)
     {
-      /* Choose the larger one. */
+      /* If a conflict occurs with the reset packet,
+       * the packet is transferred by the size specified
+       * by the receiving side. Discard the data on the sending side. */
 
-      if (spidev->tx_param.total_size < spidev->rx_param.total_size)
-        {
-          xfer_size = spidev->rx_param.total_size;
-        }
-      else
-        {
-          xfer_size = spidev->tx_param.total_size;
-        }
+      xfer_size = spidev->rx_param.total_size;
 
       /* Get DMA transfer size */
 
@@ -1251,7 +1246,7 @@ static int do_trxreset(FAR struct altmdm_dev_s *priv)
 
       /* Do DMA transfer */
 
-      ret = do_dmaxfer(priv, (void *)spidev->tx_param.buff_addr,
+      ret = do_dmaxfer(priv, (void *)g_tmp_txbuff,
                        (void *)g_tmp_rxbuff, dma_xfer_size);
     }
 
