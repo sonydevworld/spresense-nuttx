3934fcf2f950c3081c5ac9d47444e6ebf2616268
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2020-04-17 10:00:22 +0900

    Fix compile warnings in specific drivers

--- boards/arm/cxd56xx/drivers/audio/cxd56_audio_analog.c
+++ boards/arm/cxd56xx/drivers/audio/cxd56_audio_analog.c
@@ -49,20 +49,13 @@
 #include "cxd56_audio_config.h"
 #include "cxd56_audio_analog.h"
 #include "cxd56_audio_aca.h"
+#include "cxd56_clock.h"
 
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
 
 #define AUD_MCLK_EXT     (0u<<16) /* External XTAL */
-
-/****************************************************************************
- * Public Function Prototypes
- ****************************************************************************/
-
-inline void cxd56_audio_clock_enable(uint32_t clk, uint32_t div);
-inline void cxd56_audio_clock_disable(void);
-inline bool cxd56_audio_clock_is_enabled(void);
 
 /****************************************************************************
  * Private Data
