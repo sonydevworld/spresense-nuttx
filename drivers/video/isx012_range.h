ba312da9dfcae8185550828e5be19a9a704a2786
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2021-06-15 10:24:30 +0900

    drivers/video/isx012: Move it to general driver directory
    ISX012 device is general image sensor controlled via I2C.
    This driver can be used not only by Spresense.

--- drivers/video/isx012_range.h
+++ drivers/video/isx012_range.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * include/nuttx/video/isx012_range.h
+ * drivers/video/isx012_range.h
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
--- drivers/video/isx012_reg.h
+++ drivers/video/isx012_reg.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * include/nuttx/video/isx012_reg.h
+ * drivers/video/isx012_reg.h
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
