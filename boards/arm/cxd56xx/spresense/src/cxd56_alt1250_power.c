a07358b886c9e5765c9fba80a968e4c052ce063d
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2021-10-25 19:49:35 +0900

    drivers/modem/alt1250: Revise the power control sequence
    Fixed based on the following two specifications.
    
    1. Do not assert if do not use PMU_POWER_BUTTON.
    
    2. The PMU_SHUTDOWN pin is used to initiate a power cycle
    that resets the device. It should be asserted for a duration
    of >100usec to perform a hard reset.
    
    The past reset sequence used the power off and power on
    interfaces. This operation is not a correct reset sequence,
    so the reset interface was added.

--- boards/arm/cxd56xx/spresense/src/cxd56_alt1250_power.c
+++ boards/arm/cxd56xx/spresense/src/cxd56_alt1250_power.c
@@ -33,9 +33,23 @@
 #include <nuttx/board.h>
 #include <nuttx/spi/spi.h>
 #include <nuttx/modem/alt1250.h>
+#include <nuttx/wdog.h>
 #include <arch/board/board.h>
 #include "cxd56_gpio.h"
 #include "cxd56_pinconfig.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define RESET_INTERVAL_TIMEOUT MSEC2TICK(1)
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static struct wdog_s g_reset_wd;
+static sem_t g_wd_wait;
 
 /****************************************************************************
  * Public Functions
@@ -53,11 +67,11 @@ void board_alt1250_poweron(void)
 {
   /* Power on altair modem device */
 
+  cxd56_gpio_config(ALT1250_LTE_POWER_BUTTON, false);
+  cxd56_gpio_write(ALT1250_LTE_POWER_BUTTON, false);
+
   cxd56_gpio_config(ALT1250_SHUTDOWN, false);
   cxd56_gpio_write(ALT1250_SHUTDOWN, true);
-
-  cxd56_gpio_config(ALT1250_LTE_POWER_BUTTON, false);
-  cxd56_gpio_write(ALT1250_LTE_POWER_BUTTON, true);
 
   board_power_control(POWER_LTE, true);
 }
@@ -74,12 +88,56 @@ void board_alt1250_poweroff(void)
 {
   /* Power off Altair modem device */
 
-  cxd56_gpio_write(ALT1250_SHUTDOWN, true);
-
   board_power_control(POWER_LTE, false);
 
   cxd56_gpio_write(ALT1250_SHUTDOWN, false);
   cxd56_gpio_write(ALT1250_LTE_POWER_BUTTON, false);
+}
+
+/****************************************************************************
+ * Name: board_alt1250_timeout
+ *
+ * Description:
+ *   Watchdog timer for timeout of reset interval.
+ *
+ ****************************************************************************/
+
+static void board_alt1250_timeout(wdparm_t arg)
+{
+  sem_t *wd_wait = (sem_t *)arg;
+
+  nxsem_post(wd_wait);
+}
+
+/****************************************************************************
+ * Name: board_alt1250_reset
+ *
+ * Description:
+ *   Reset the Altair modem device on the board.
+ *
+ ****************************************************************************/
+
+void board_alt1250_reset(void)
+{
+  memset(&g_reset_wd, 0, sizeof(struct wdog_s));
+  nxsem_init(&g_wd_wait, 0, 0);
+
+  /* Reset Altair modem device */
+
+  cxd56_gpio_write(ALT1250_SHUTDOWN, false);
+
+  /* ALT1250_SHUTDOWN should be low in the range 101usec to 100msec */
+
+  wd_start(&g_reset_wd, RESET_INTERVAL_TIMEOUT,
+           board_alt1250_timeout, (wdparm_t)&g_wd_wait);
+
+  /* Wait for the watchdog timer to expire */
+
+  nxsem_wait_uninterruptible(&g_wd_wait);
+
+  cxd56_gpio_write(ALT1250_SHUTDOWN, true);
+
+  nxsem_destroy(&g_wd_wait);
 }
 
 #endif
