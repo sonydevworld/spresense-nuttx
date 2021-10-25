fbac19e8e33b4bceb6205a49e2051b02ff0b5e33
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2021-10-25 19:49:35 +0900

    drivers/modem/alt1250: Revise the power control sequence
    Fixed based on the following two specifications.
    
    1. The PMU_SHUTDOWN and PMU_POWER_BUTTON pins should not be
    asserted during the power up and can be used only >100msec
    after PMU_VBAT_LDO and PMU_VBAT_DCDC are stable.
    
    2. The PMU_SHUTDOWN pin is used to initiate a power cycle
    that resets the device. It should be asserted for a duration
    of >100usec to perform a hard reset.
    
    The past reset sequence used the power off and power on
    interfaces. This operation is not a correct reset sequence,
    so the reset interface was added.

--- boards/arm/cxd56xx/spresense/src/cxd56_alt1250_power.c
+++ boards/arm/cxd56xx/spresense/src/cxd56_alt1250_power.c
@@ -34,8 +34,16 @@
 #include <nuttx/spi/spi.h>
 #include <nuttx/modem/alt1250.h>
 #include <arch/board/board.h>
+#include <nuttx/signal.h>
 #include "cxd56_gpio.h"
 #include "cxd56_pinconfig.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define POWERON_INTERVAL_NSEC (100 * 1000 * 1000)
+#define POWEROFF_INTERVAL_NSEC (100 * 1000)
 
 /****************************************************************************
  * Public Functions
@@ -51,15 +59,19 @@
 
 void board_alt1250_poweron(void)
 {
+  struct timespec interval =
+    {
+      0, POWERON_INTERVAL_NSEC
+    };
+
   /* Power on altair modem device */
 
-  cxd56_gpio_config(ALT1250_SHUTDOWN, false);
-  cxd56_gpio_write(ALT1250_SHUTDOWN, true);
-
-  cxd56_gpio_config(ALT1250_LTE_POWER_BUTTON, false);
-  cxd56_gpio_write(ALT1250_LTE_POWER_BUTTON, true);
-
   board_power_control(POWER_LTE, true);
+
+  nxsig_nanosleep(&interval, NULL);
+
+  cxd56_gpio_write(ALT1250_SHUTDOWN, true);
+  cxd56_gpio_write(ALT1250_LTE_POWER_BUTTON, true);
 }
 
 /****************************************************************************
@@ -72,14 +84,48 @@ void board_alt1250_poweron(void)
 
 void board_alt1250_poweroff(void)
 {
+  struct timespec interval =
+    {
+      0, POWEROFF_INTERVAL_NSEC
+    };
+
   /* Power off Altair modem device */
 
-  cxd56_gpio_write(ALT1250_SHUTDOWN, true);
+  cxd56_gpio_write(ALT1250_LTE_POWER_BUTTON, false);
+  cxd56_gpio_write(ALT1250_SHUTDOWN, false);
+
+  nxsig_nanosleep(&interval, NULL);
 
   board_power_control(POWER_LTE, false);
 
+  /* Output disable */
+
+  cxd56_gpio_config(ALT1250_LTE_POWER_BUTTON, false);
+  cxd56_gpio_config(ALT1250_SHUTDOWN, false);
+}
+
+/****************************************************************************
+ * Name: board_alt1250_reset
+ *
+ * Description:
+ *   Reset the Altair modem device on the board.
+ *
+ ****************************************************************************/
+
+void board_alt1250_reset(void)
+{
+  struct timespec interval =
+    {
+      0, POWEROFF_INTERVAL_NSEC
+    };
+
+  /* Reset Altair modem device */
+
   cxd56_gpio_write(ALT1250_SHUTDOWN, false);
-  cxd56_gpio_write(ALT1250_LTE_POWER_BUTTON, false);
+
+  nxsig_nanosleep(&interval, NULL);
+
+  cxd56_gpio_write(ALT1250_SHUTDOWN, true);
 }
 
 #endif
